43fbf3003bc5976385113b522382b4a38589fed7
Jose <jdelsol20@gmail.com>
2023-06-16 02:36:58 -0700

    Updated fetch URL

--- src/componets/ContactList.jsx
+++ src/componets/ContactList.jsx
@@ -16,7 +16,7 @@ export default function ContactList() {
         async function fetchContacts() {
             try {
                 const response = await fetch(
-                    "https://jsonplace-univclone.herokuapp.com/users"
+                    "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
                   );
                   const result = await response.json();
                   setContacts(result);
